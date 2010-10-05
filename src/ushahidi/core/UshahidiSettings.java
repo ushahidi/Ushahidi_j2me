dcb74df1ca40e43ef8d48824141ae17a67bb8e54
Edmund Ocheing <stuart@weasel.wild.ly>
2010-10-05 15:42:59 +0300

    Added icon.png image Added UshahidiSettings.java class for handling of user settings

---
+++ src/ushahidi/core/UshahidiSettings.java
@@ -0,0 +1,267 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ushahidi.core;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.util.Vector;
+import javax.microedition.rms.InvalidRecordIDException;
+import javax.microedition.rms.RecordStore;
+import javax.microedition.rms.RecordStoreException;
+import javax.microedition.rms.RecordStoreNotOpenException;
+
+/**
+ *
+ * @author stuart
+ */
+public class UshahidiSettings {
+
+    private RecordStore getRecordStore(String recordStore) {
+        RecordStore instanceStore = null;
+
+        try {
+            instanceStore = RecordStore.openRecordStore(recordStore, true);
+        } catch (RecordStoreException e) {
+            System.err.println(e.getMessage());
+        }
+
+        return instanceStore;
+    }
+
+    public void setUshahidiDeployment() {
+        RecordStore rs = null;
+        String instanceAddress = null;
+
+        try {
+            rs = getRecordStore("CurrentInstance");
+            if (rs.getNumRecords() == 0) {
+                instanceAddress = "http://demo.ushahidi.com";
+                saveInstance("Demo", "http://demo.ushahidi.com");
+            }     else instanceAddress = this.getCurrentInstance();
+
+
+        } catch (Exception ex) {
+            System.err.println(ex.getMessage());
+        } finally {
+            UshahidiInstance.setUshahidiInstance(instanceAddress);
+        }
+    }
+
+    public int saveSettings(int index, String numberOfReports, String firstName, String lastName, String email) {
+        RecordStore rs = null;
+        int recordID = 0;
+
+        try {
+            rs = getRecordStore("SettingsDB");
+            ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+            DataOutputStream writer = new DataOutputStream(byteStream);
+            writer.writeUTF(String.valueOf(index)); // Instance address -> Index in ComboBox
+            writer.writeUTF(numberOfReports); // Number of reports
+            writer.writeUTF(firstName); // First name
+            writer.writeUTF(lastName); //Last Name
+            writer.writeUTF(email); // E-mail address
+            writer.flush();
+
+            byte[] record = byteStream.toByteArray();
+            if (rs.getNumRecords() == 0)
+                recordID = rs.addRecord(record, 0, record.length);
+            else rs.setRecord(1, record, 0, record.length);
+
+            writer.close();
+            byteStream.close();
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        } finally {
+            closeRecordStore(rs);
+        }
+        return recordID;
+    }
+
+    public String[] getSettings() {
+        ByteArrayInputStream inputByteStream;
+        DataInputStream reader;
+        String[] userSetting = null;
+
+        try {
+            RecordStore rs = getRecordStore("SettingsDB");
+
+            try {
+                byte[] settings = rs.getRecord(1);
+                inputByteStream = new ByteArrayInputStream(settings);
+                reader = new DataInputStream(inputByteStream);
+
+                if (rs.getNumRecords()  != 0) {
+                    userSetting = new String[5];
+
+                    userSetting[0] = reader.readUTF(); // Instance address
+                    userSetting[1] = reader.readUTF(); // Number of reports
+                    userSetting[2] = reader.readUTF(); // First name
+                    userSetting[3] = reader.readUTF(); //Last Name
+                    userSetting[4] = reader.readUTF(); // E-mail address
+                } //end if
+
+            } catch (InvalidRecordIDException ex) {
+                System.err.println(ex.getMessage());
+            }
+
+
+        } catch (RecordStoreNotOpenException ex) {
+            System.err.println(ex);
+        } catch (Exception ex) {
+            System.err.println(ex);
+        }
+
+        return userSetting;
+    }
+
+    public int saveInstance(String instanceTitle, String instanceAddress) {
+        RecordStore rs = null;
+        int recordID = 0;
+
+        try {
+
+            rs = getRecordStore("InstancesDB");
+            ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+            DataOutputStream writer = new DataOutputStream(byteStream);
+
+            writer.writeUTF(instanceTitle); // Instance name
+            writer.writeUTF(instanceAddress); // Instance address
+            writer.flush();
+
+            byte[] record = byteStream.toByteArray();
+            recordID = rs.addRecord(record, 0, record.length);
+
+            writer.close();
+            byteStream.close();
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        } finally {
+            closeRecordStore(rs);
+        }
+        
+        return recordID;
+    }
+
+    public Vector getAllInstances() {
+        Vector instances = new Vector();
+//        String activeInstance = null;
+        RecordStore rs = null;
+        ByteArrayInputStream byteStream = null;
+        DataInputStream reader = null;
+
+        try {
+            rs = getRecordStore("InstancesDB");
+
+                for (int i = 1; i <= rs.getNumRecords(); i++) {
+                    byte[] data = rs.getRecord(i);
+                    byteStream = new ByteArrayInputStream(data);
+                    reader = new DataInputStream(byteStream);
+
+                    String deploymentName = reader.readUTF();
+                    String activeInstance = reader.readUTF();
+
+                    instances.addElement(new String[]{deploymentName, activeInstance});
+
+                    // Close streams
+                    reader.close();
+                    byteStream.close();
+                } // end for
+
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+
+        return instances;
+    }
+
+    public String[] getTitles() {
+        String[] titles = null;
+        Vector instancesVector = getAllInstances();
+
+        titles = new String[instancesVector.size()];
+
+        for (int i = 0; i < instancesVector.size(); i++) {
+            String instance[] = (String[]) instancesVector.elementAt(i);
+            titles[i] = instance[0];
+        }
+
+        return titles;
+    }
+
+    public void getInstanceAddressByName(Object name) {
+        String url = null;
+        Vector instances = getAllInstances();
+
+        for (int i = 0; i < instances.size(); i++) {
+            String[] instance = (String[]) instances.elementAt(i);
+            if (instance[0].equals(name)) url = instance[1];
+        }
+
+        UshahidiInstance.setUshahidiInstance(url);
+    }
+
+    public int saveCurrentInstance() {
+        RecordStore rs = null;
+        int recordID = 0;
+
+        try {
+            rs = getRecordStore("CurrentInstance");
+            ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+            DataOutputStream writer = new DataOutputStream(byteStream);
+            writer.writeUTF(UshahidiInstance.getUshahidiInstance()); // Address of active Instance
+            writer.flush();
+
+            byte[] record = byteStream.toByteArray();
+
+            if (rs.getNumRecords() == 0) {
+                recordID = rs.addRecord(record, 0, record.length);
+            } else {
+                rs.setRecord(1, record, 0, record.length);
+            }
+
+            writer.close();
+            byteStream.close();
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        } finally {
+            closeRecordStore(rs);
+        }
+        return recordID;
+    }
+
+    public String getCurrentInstance() {
+        String currentInstance = null;
+        ByteArrayInputStream byteStream = null;
+        DataInputStream reader = null;
+        RecordStore rs = null;
+
+        try {
+            rs = getRecordStore("CurrentInstance");
+
+            byte[] data = rs.getRecord(1);
+            byteStream = new ByteArrayInputStream(data);
+            reader = new DataInputStream(byteStream);
+            currentInstance = reader.readUTF().toString();
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        } finally {
+            closeRecordStore(rs);
+        }
+
+        return currentInstance;
+    }
+
+    private void closeRecordStore(RecordStore recordStore) {
+        if (recordStore != null) {
+            try {
+                recordStore.closeRecordStore();
+            } catch (RecordStoreException e) {
+                System.err.println(e.getMessage());
+            }
+        } //end if
+    }
+}
